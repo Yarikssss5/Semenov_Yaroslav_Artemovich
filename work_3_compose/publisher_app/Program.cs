31bb9a4d31ab6d6c9cc44b66b78fbe8dbca13a46
Yaroslav <114652956+Yarikssss5@users.noreply.github.com>
2025-11-24 22:43:49 +0500

    work_3_compose part 3 added

---
+++ work_3_compose/publisher_app/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+class Lesson
+{
+    public int Count {get; set; }
+    public string Office {get; set;}
+    public string Teacher {get; set;}
+    public string Subject {get; set;}
+    public string Time {get; set;}
+
+    public Lesson(int count, string office, string teacher, string subject, string time)
+    {
+        Count = count;
+        Office = office;
+        Teacher = teacher;
+        Subject = subject;
+        Time = time;
+    }
+}
+class Producer
+{
+    static void Main()
+    {
+        // Читаем параметры подключения из переменных окружения
+        var host = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "localhost";
+        var user = Environment.GetEnvironmentVariable("RABBITMQ_USER") ?? "guest";
+        var pass = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") ?? "guest";
+        // Расписание
+        var lessons = new List<Lesson>
+        {
+            new Lesson(1, "101", "Иванов И.И.", "Математика", "08:00"),
+            new Lesson(2, "202", "Петров П.П.", "Физика", "09:30"),
+            new Lesson(3, "303", "Сидоров С.С.", "История", "11:00"),
+            new Lesson(4, "404", "Кузнецов К.К.", "Программирование", "13:00"),
+            new Lesson(5, "404", "Кузнецов К.К.", "Программирование", "17:01"),
+            new Lesson(6, "505", "Смирнова А.А.", "Английский", "22:30")
+        };
+        var delays = new List<int> {8, 9, 11, 13, 14, 16};
+        // Создаем подключение к RabbitMQ
+        var factory = new ConnectionFactory() { HostName = host, UserName = user, Password = pass };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel()) {
+            // Создаем очередь с именем "lessons"
+            channel.QueueDeclare("lessons", false, false, false, null);
+            var timeout = 0;
+            foreach (var lesson in lessons)
+            {
+                timeout = (timeout + 1) % delays.Count;
+                Thread.Sleep(1000 * 60 * delays[timeout]);
+                string message = JsonSerializer.Serialize(lesson);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "", 
+                    routingKey: "lessons", 
+                    basicProperties: null,
+                    body: body
+                );
+            }
+        }
+    }
+}
---
+++ work_3_compose/subscriber_app/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Lesson
+{
+    public int Count {get; set; }
+    public string Office {get; set;}
+    public string Teacher {get; set;}
+    public string Subject {get; set;}
+    public string Time {get; set;}
+    public Lesson(int count, string office, string teacher, string subject, string time)
+    {
+        Count = count;
+        Office = office;
+        Teacher = teacher;
+        Subject = subject;
+        Time = time;
+    }
+}
+
+class Consumer
+{
+    static void Main()
+    {
+        // Читаем параметры подключения из переменных окружения
+        var host = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "localhost";
+        var user = Environment.GetEnvironmentVariable("RABBITMQ_USER") ?? "guest";
+        var pass = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") ?? "guest";
+        // Создаем подключение к RabbitMQ
+        var factory = new ConnectionFactory()
+        {
+            HostName = host, // Адрес сервера RabbitMQ
+            UserName = user,
+            Password = pass
+        };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("lessons", false, false, false, null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var json = Encoding.UTF8.GetString(body);
+                var lesson = JsonSerializer.Deserialize<Lesson>(json);
+                if (lesson == null) { }
+                else {
+                    Console.WriteLine($"[x] Получено: {lesson.Subject} | {lesson.Office} | {lesson.Teacher} | {lesson.Time}");
+                }
+            };
+            channel.BasicConsume("lessons", true, consumer);
+            Console.WriteLine("Ожидаем пары...");
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+} 
